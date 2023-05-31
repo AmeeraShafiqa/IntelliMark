a19a4cbc46aaf75d3952f13d874180516a10654d
Meera <ameeraashafiqaa@gmail.com>
2023-06-01 04:39:46 +0800

    Add project files.

---
+++ src/Application/Common/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using CleanArchitecture.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchitecture.Application.Common.Interfaces;
+
+public interface IApplicationDbContext
+{
+    DbSet<Table1> Table1 { get; set; }
+    DbSet<Table2> Table2 { get; set; }
+    DbSet<Table3> Table3 { get; set; }
+
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+}
